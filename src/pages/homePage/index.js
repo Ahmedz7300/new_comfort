75ee5c4070cf08db60b062f2a4c1ab01e60d1523
ahmedz7300 <ahmed.khan7300@gmail.com>
2023-12-27 13:37:30 -0800

    aimz

---
+++ src/pages/homePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div><h1>Java</h1>
+    <h3>React js and Node js  in batch 803</h3>
+    </div>
+    
+  )
+}
+
+
+export default HomePage
